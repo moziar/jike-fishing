f551d8c260e43f303a3e6f0e6ec9b05b33496301
moziar <cortex-z@qq.com>
2019-04-08 17:47:43 +0800

    simple mile stone

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   static String _logoPathDefault = 'images/logo.png';
 
-  static String _logo2 = 'images/logo2.png';    // a
-  static String _logo3 = 'images/logo3.png';    // fishing
-  static String _logo4 = 'images/logo4.png';    // Sube sube sube
-  static String _logo5 = 'images/logo5.png';    // Sube el volumen
+  static String _logo2 = 'images/logo2.png'; // a
+  static String _logo3 = 'images/logo3.png'; // fishing
+  static String _logo4 = 'images/logo4.png'; // Sube sube sube
+  static String _logo5 = 'images/logo5.png'; // Sube el volumen
 
-  String _logoPath1 = _logoPathDefault;   // the 1st photo
-  String _logoPath2 = _logoPathDefault;   // the 2st photo
+  String _logoPath1 = _logoPathDefault; // the 1st photo
+  String _logoPath2 = _logoPathDefault; // the 2st photo
 
   String _logoPath = _logoPathDefault; // Set Default Path
 
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       } else if (20 <= _counter && _counter < 40) {
         _logoPath1 = '$_logo2';
         _logoPath2 = '$_logo3';
-      } else if (40 <= _counter && _counter < 80){
+      } else if (40 <= _counter && _counter < 80) {
         _logoPath1 = '$_logo4';
         _logoPath2 = '$_logo5';
       }
@@ -87,40 +87,46 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text.rich(TextSpan(children: [
-              TextSpan(
-                  text: '今天已经摸了  ',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 135, 135, 135),
-                    fontSize: 17.0,
-                  )),
-              TextSpan(
-                text: '$_counter',
-                style: Theme.of(context).textTheme.display1,
-              ),
-              TextSpan(
-                  text: '  次鱼',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 135, 135, 135),
-                    fontSize: 17.0,
-                  )),
-            ])),
-            FlatButton(
-              onPressed: () => {_incrementCounter(), _changePhoto()},
-              splashColor: Color(0x00000000),
-              color: Color(0x00000000),
-              highlightColor: Color(0x00000000),
-              child: Image(
-                image: AssetImage('$_logoPath'),
-                width: 230.0,
-                height: 220.0,
-              ),
-            ),
-          ],
-        ),
+        child: ListView(shrinkWrap: true, itemExtent: 640, children: <Widget>[
+          Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Text.rich(
+                  TextSpan(children: [
+                TextSpan(
+                    text: '今天已经摸了  ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 135, 135, 135),
+                      fontSize: 17.0,
+                    )),
+                TextSpan(
+                  text: '$_counter',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+                TextSpan(
+                    text: '  次鱼',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 135, 135, 135),
+                      fontSize: 17.0,
+                    )),
+              ])),
+              Container(
+                alignment: Alignment.center,
+                  constraints: BoxConstraints.expand(),
+                  child: FlatButton(
+                onPressed: () => {_incrementCounter(), _changePhoto()},
+                splashColor: Color(0x00000000),
+                color: Color(0x00000000),
+                highlightColor: Color(0x00000000),
+                child: Image(
+                  image: AssetImage('$_logoPath'),
+                  width: 230.0,
+                  height: 220.0,
+                ),
+              )),
+            ],
+          )
+        ]),
       ),
     );
   }
